c8706db4ab9d68fbf43e0e98ba40a1c115fec7df
Sagleft <sagleft@inbox.ru>
2019-06-08 13:42:02 +0400

    схема структуры и небольшие улучшения

--- controller/src/LogicController.php
+++ controller/src/LogicController.php
@@ -10,16 +10,31 @@
 			$this->renderT = new \Vote\Controller\Render();
 		}
 		
-		public function get_render() {
-			return $this->renderT;
-		}
+		//public function get_render() {
+		//	return $this->renderT;
+		//}
 		
 		public function render($data) {
 			$this->renderT->twigRender($data);
 		}
 		
+		public function renderModulePage($module, $renderData) {
+			$this->renderT->twigRender([
+				'tag'    => 'module',
+				'module' => [
+					'name' => $module->name, 'room' => $renderData['room']
+				],
+				'title'  => 'Мой профиль',
+				'user'   => [
+					'uid'       => $this->uid,
+					'nick_name' => $this->nick_name,
+					'is_auth'   => $this->is_auth
+				],
+				'lang'  => $this->language
+			]);
+		}
+		
 		public function renderPage($tag) {
-			//TODO: убрать или изменить эту функцию
 			$this->renderT->twigRender([
 				'tag'  => $tag
 			]);
--- model/Logic.php
+++ model/Logic.php
@@ -5,15 +5,12 @@
 		public  $controller    = null;
 		public $modulesManager = null;
 		private $environment   = null;
-		//private $user = null;
 		
 		public function __construct() {
 			$this->controller     = new \Vote\Controller\LogicController();
 			$renderT = $this->controller->get_render();
 			$this->modulesManager = new \Vote\Model\ModulesManager($renderT);
 			$this->environment    = new \Vote\Model\Environment();
-			
-			//$this->user = $this->modulesManager->getModule('user');
 		}
 	}
 	
